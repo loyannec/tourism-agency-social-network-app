4d6b5becc867a344502d52cbe0bedcb2ff195438
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-11 15:58:24 +0100

    included a first router

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const indexRouter = require("./routes/index");
+
 require('./plugins/express.js')(app);
 require('./plugins/cookie-parser')(app);
 require('./plugins/livereload')(app);
 require('./plugins/handlebars')(app);
 require('./plugins/http-errors')(app);
 require('./plugins/morgan')(app);
+
+app.use("/", indexRouter);
 
 /*
 Testing if start server
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+module.exports = (app) {
+const express = require
+
+}
