a75decfab5c437c6e7e87808b6e6c2709876d7fd
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-11 15:01:27 +0100

    installed http-errors

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 require('./plugins/express.js')(app);
-require('./plugins/cookieParser')(app);
+require('./plugins/cookie-parser')(app);
 require('./plugins/livereload')(app);
 require('./plugins/handlebars')(app);
+require('./plugins/http-errors')(app);
 
 console.log("Starting server");
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
--- plugins/cookieParser.js
+++
@@ -1,8 +0,0 @@
-/*
-Middleware which parses cookies attached to the client request object
-*/
-module.exports = (app) => {
-    const cookieParser = require('cookie-parser');
-
-    app.use(cookieParser());
-};
