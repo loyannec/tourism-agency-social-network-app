86fc21b61cad0e6d72da901546d8d1fb1cbf55e9
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-12 15:57:42 +0100

    added some routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,43 @@
 module.exports = (app) {
 const express = require
 
-}
+/*
+GET home page.
+*/
+app.get('/');
+
+/*
+Display search location
+*/
+app.get('/filterlocation');
+
+/*
+Log out user and display login page.
+*/
+app.get('/logout');
+
+/*
+Display respective Agency Page
+*/
+app.get('/gdfgf/:id');
+
+/*
+Display Register page
+*/
+app.get('/register');
+
+/*
+Submits User details
+*/
+app.post('/register');
+
+/*
+Display search by locations
+*/
+app.get('/searchjdjdjdj');
+
+/*
+Display respective Agency Page
+*/
+app.post('/reviews');
+};
