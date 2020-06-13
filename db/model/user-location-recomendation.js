63e93093a09634e447c5e7cab300d6f92fe78c53
Bharti4 <bharti.madnani@gmail.com>
2020-06-13 19:12:02 +0530

    user Location RecomendationSchema schema

--- db/model/user-location-recomendation.js
+++ db/model/user-location-recomendation.js
@@ -4,6 +4,24 @@ const mongoose = require("mongoose");
 Locations schema
 */
 const userLocationRecomendationSchema = mongoose.Schema({
+    userId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'user', 
+        required: true
+        },
+    locationId :{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'location', 
+        required: true
+        },
+    comments: {
+        type: String,
+        required: true
+        },
+    recomends: {
+        type: Boolean,
+        required: true
+    }
 
 });
 
