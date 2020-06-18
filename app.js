d17a23a767de66ae590307b1ca9aef2c76e5b121
Bharti4 <bharti.madnani@gmail.com>
2020-06-18 23:00:49 +0530

    added code to connect mongodb at application level

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+var mongoDB = require("./db/db-connection");
 const loadRoutes = require("./routes");
 require("./plugins")(app);
 
@@ -10,5 +10,7 @@ loadRoutes(app);
 /*
 Testing if start server
 */
+const db = mongoDB(()=>{
 console.log("Starting server");
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+})
