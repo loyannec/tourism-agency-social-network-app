d54f18ce9b3cffdf15e58dc759d7d64d08d82111
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-20 10:18:27 +0100

    Add location details files

--- routes/details.js
+++ routes/details.js
@@ -1,22 +1,29 @@
+const Location = require("../db/models/location");
+
 module.exports = (app) => {
+    /*
+    Display respective details page
+    */
+    app.get('/location/:id', (req, res) => {
+        Location.findById(req.params.id, (err, location) => {
+            if (err) {
+                res.send(404);
+                return;
+            }
+            res.render('location/details', { location });
+        }).lean();
+    });
     /*
     Display respective comments page
     */
-    app.get('/location/comments', (req, res) => {
+    app.get('/location/:id/comments', (req, res) => {
         res.render('details');
     });
 
     /*
     Display user recommend page
     */
-    app.get('/location/recommend', (req, res) => {
-        res.render('details');
-    });
-
-    /*
-    Display respective details page
-    */
-    app.get('/location/:id', (req, res) => {
+    app.get('/location/:id/recommend', (req, res) => {
         res.render('details');
     });
 };
