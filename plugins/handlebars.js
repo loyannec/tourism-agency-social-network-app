4cd5312e9ff4bceff94d4abd20f745f8d425a208
Bharti4 <bharti.madnani@gmail.com>
2020-06-21 21:26:18 +0530

    added truncate handlebar helper

--- plugins/handlebars.js
+++ plugins/handlebars.js
@@ -3,9 +3,29 @@ module.exports = (app) => {
     const handlebars = exphbs.create({
         helpers: {
             imageToBase64: (data) => {
-                console.log(data);
                 var image = data.buffer.toString('base64');
                 return image;
+            },
+            truncate :(str, len)=> {
+                if (str.length > len) {
+                    var new_str = str.substr (0, len+1);
+        
+                    while (new_str.length) {
+                        var ch = new_str.substr ( -1 );
+                        new_str = new_str.substr ( 0, -1 );
+        
+                        if (ch == ' ') {
+                            break;
+                        }
+                    }
+        
+                    if ( new_str == '' ) {
+                        new_str = str.substr ( 0, len );
+                    }
+        
+                    return  new_str +'...' ;
+                }
+                return str;
             }
         }
     });
